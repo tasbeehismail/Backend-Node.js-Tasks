72380f6d6d99508fadcc2f69dd83d51c23633ad2
tasbeehismail <tsbeehismaill@gmail.com>
2024-06-08 02:32:07 +0300

    deploy app on render

--- Week#6/src/app.js
+++ Week#6/src/app.js
@@ -1,7 +1,10 @@
 import express from 'express' 
 import bootstrap from '../src/bootstrap.js'; 
+import cors from 'cors';
+
 const app = express()
 
 bootstrap(app)  
-
-app.listen(3300, () => console.log('server running on port 3300'))  
+app.use(cors());
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`server running on port ${port}$`))  
